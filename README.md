71d0219315e25e1d09225226635f846951a93fd8
Sandor Tamas <thomas.sandor@gmail.com>
2022-08-17 17:08:01 +0200

    add wifi status

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # home_assistant
+
+Start MQTT process
+```
+./mqtt.sh &>/dev/null &
+```
+Check if process started
+```
+ps | grep mqtt
+```
+Example of a started service:
+```
+root@OpenWrt:/# ps | grep mqtt
+16660 root      1276 S    /bin/sh ./mqtt.sh
+
+```
--- openwrt_mqtt.sh
+++ openwrt_mqtt.sh
@@ -5,7 +5,7 @@ MQTTUSER=mqttuser
 MQTTPASS=mqttuser
 PORT=1883
 INTERVAL=10
-DBG=false
+DBG=true
 
 [ -x /usr/bin/rrdtool ] || exit 0
 
@@ -35,8 +35,22 @@ mosquitto_pub -h $MQTTSERVER -p $PORT -u $MQTTUSER -P $MQTTPASS -t "homeassistan
 mosquitto_pub -h $MQTTSERVER -p $PORT -u $MQTTUSER -P $MQTTPASS -t "homeassistant/sensor/router/cpu/config" -m "{\
 \"name\":\"cpu_$name\"\
 ,\"state_topic\":\"router/status\"\
-,\"icon\":\"mdi:cpu-64-bit\",\"unit_of_measurement\":\"%\",\"value_template\":\"{{ value_json.cpu }}\"\
+,\"icon\":\"mdi:cpu-32-bit\",\"unit_of_measurement\":\"%\",\"value_template\":\"{{ value_json.cpu }}\"\
 ,\"unique_id\":\"cpu_$name\"\
+}" -i "OpenWRT" -r
+#discover wifi clients sensor
+mosquitto_pub -h $MQTTSERVER -p $PORT -u $MQTTUSER -P $MQTTPASS -t "homeassistant/sensor/router/wificlients/config" -m "{\
+\"name\":\"wifi_clients_$name\"\
+,\"state_topic\":\"router/status\"\
+,\"icon\":\"mdi:account-voice\",\"unit_of_measurement\":\"db\",\"value_template\":\"{{ value_json.wifi_clients }}\"\
+,\"unique_id\":\"wifi_clients_$name\"\
+}" -i "OpenWRT" -r
+#discover wifi quality sensor
+mosquitto_pub -h $MQTTSERVER -p $PORT -u $MQTTUSER -P $MQTTPASS -t "homeassistant/sensor/router/wifiquality/config" -m "{\
+\"name\":\"wifi_quality_$name\"\
+,\"state_topic\":\"router/status\"\
+,\"icon\":\"mdi:wifi-check\",\"unit_of_measurement\":\"%\",\"value_template\":\"{{ value_json.wifi_quality }}\"\
+,\"unique_id\":\"wifi_quality_$name\"\
 }" -i "OpenWRT" -r
 #discover memory free sensor
 mosquitto_pub -h $MQTTSERVER -p $PORT -u $MQTTUSER -P $MQTTPASS -t "homeassistant/sensor/router/memoryfree/config" -m "{\
@@ -87,8 +101,23 @@ MEMORY_USED=$(rrdtool fetch /tmp/rrd/OpenWrt/memory/memory-used.rrd AVERAGE -s -
 #CPU
 CPU=$(rrdtool fetch /tmp/rrd/OpenWrt/cpu/percent-active.rrd AVERAGE -s -30s | grep -v nan | tail -1 | awk '{printf("%.1f\n", $2)}')
 
+#WIFI 2.4GHz
+WLAN0_CLIENTS=$(rrdtool fetch /tmp/rrd/OpenWrt/iwinfo-wlan0/stations.rrd AVERAGE -s -30s | grep -v nan | tail -1 | awk '{printf("%.0f\n", $2)}')
+WLAN0_QUALITY=$(rrdtool fetch /tmp/rrd/OpenWrt/iwinfo-wlan0/signal_quality.rrd AVERAGE -s -30s | grep -v nan | tail -1 | awk '{printf("%.1f\n", $2)}')
+
+#WIFI 5GHz
+WLAN1_CLIENTS=$(rrdtool fetch /tmp/rrd/OpenWrt/iwinfo-wlan1/stations.rrd AVERAGE -s -30s | grep -v nan | tail -1 | awk '{printf("%.0f\n", $2)}')
+WLAN1_QUALITY=$(rrdtool fetch /tmp/rrd/OpenWrt/iwinfo-wlan1/signal_quality.rrd AVERAGE -s -30s | grep -v nan | tail -1 | awk '{printf("%.1f\n", $2)}')
+
+WLAN_CLIENTS=$(echo "$WLAN0_CLIENTS+$WLAN1_CLIENTS" | bc -l)
+WLAN_QUALITY=$(echo "$WLAN0_QUALITY+$WLAN1_QUALITY" | bc -l)
+
+if [ $(echo "$WLAN0_QUALITY > 0" | bc) -gt 0 ] && [ $(echo "$WLAN1_QUALITY > 0" | bc) -gt 0 ]; then
+    WLAN_QUALITY=$(echo "$WLAN_QUALITY / 2.0" | bc -l | xargs printf '%.1f')
+fi
+
 if $DBG; then
-    echo "$TIME $WAN_RX $WAN_TX $CPU $MEMORY_FREE $MEMORY_BUFFERED $MEMORY_CACHED $MEMORY_USED"
+    echo "$TIME $WAN_RX $WAN_TX $CPU $WLAN_CLIENTS $WLAN_QUALITY $MEMORY_FREE $MEMORY_BUFFERED $MEMORY_CACHED $MEMORY_USED"
 fi
 
 mosquitto_pub -h $MQTTSERVER -u $MQTTUSER -P $MQTTPASS -t router/status -m "{\
@@ -96,6 +125,8 @@ mosquitto_pub -h $MQTTSERVER -u $MQTTUSER -P $MQTTPASS -t router/status -m "{\
 ,\"wan_rx\":\"$WAN_RX\"\
 ,\"wan_tx\":\"$WAN_TX\"\
 ,\"cpu\":\"$CPU\"\
+,\"wifi_clients\":\"$WLAN_CLIENTS\"\
+,\"wifi_quality\":\"$WLAN_QUALITY\"\
 ,\"memory_free\":\"$MEMORY_FREE\"\
 ,\"memory_buffered\":\"$MEMORY_BUFFERED\"\
 ,\"memory_cached\":\"$MEMORY_CACHED\"\
